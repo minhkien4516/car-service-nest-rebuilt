d9542b20a76f3b20f8a15ea8cdfa7781af1c132e
ptanlam <phanlam20033002@gmail.com>
2021-05-16 10:55:55 +0700

    add packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({}),
+    SequelizeModule.forRoot({
+      dialect: 'mssql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
